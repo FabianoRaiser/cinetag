21edff0db037856250a0d0865d1bd716ca4e54ef
Fabiano <fabiano.raiser16@gmail.com>
2024-02-18 15:57:21 -0300

    feat: construção do Cabecalho e do Rodape

--- src/App.jsx
+++
@@ -1,6 +0,0 @@
-
-export const App = () => {
-    return (
-        <h1>Olá Mundo</h1>
-    )
-}
---
+++ src/components/Rodape/index.jsx
@@ -0,0 +1,9 @@
+import styles from './Rodape.module.css';
+
+export default function Rodape() {
+  return (
+    <footer className={styles.rodape}>
+        <h2>Desolvido por Fabiano Raiser</h2>
+    </footer>
+  )
+}
